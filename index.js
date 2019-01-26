75e91ae3bc89a3ff06aaba13dae92c0237d3bd35
vernon <vernon.lillies@gmail.com>
2019-01-25 21:34:08 -0500

    install prompts package and add user subway line selection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const { to } = require("await-to-js");
 const process = require("process");
+const prompts = require("prompts");
 
 async function main() {
   const [err, subwayLines] = await to(getSubwayLines());
@@ -11,7 +12,11 @@ async function main() {
     );
     process.exit(0); // exit gracefully
   }
-  console.log(subwayLines);
+
+  console.log(subwayLines.join());
+  const selectedLineId = await getUserSelectedLineId(subwayLines);
+
+  console.log(selectedLineId);
 }
 
 /**
@@ -29,6 +34,20 @@ function getSubwayLines() {
         return el.id;
       });
     });
+}
+
+/**
+ * Get User Selected Subway Line Id
+ *
+ * @return {Promise} returns an objected with the selected id
+ */
+async function getUserSelectedLineId(subwayLines) {
+  return await prompts({
+    type: "text",
+    name: "selectedLineId",
+    message: "Please enter a subway line id from the above list",
+    validate: selectedLineId => subwayLines.includes(selectedLineId)
+  });
 }
 
 main().then(() => console.log("Thanks, see you next time!"));
